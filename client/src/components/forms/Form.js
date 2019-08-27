55b8c32ecd312a15b41544ce32cec6c727cf33d3
rubenberna <rubenmbernardes@gmail.com>
2019-08-27 10:42:56 +0200

    feat(): form updates state values

--- client/src/components/forms/Form.js
+++ client/src/components/forms/Form.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Form, Header } from 'semantic-ui-react'
 
-import { ReactComponent as Logo } from '../../assets/logo.svg'
 import './form.scss'
+import { ReactComponent as Logo } from '../../assets/logo.svg'
+import UiDivider from '../divider/Divider'
 
 const options = [
   { key: 'm', text: 'Male', value: 'male' },
@@ -14,7 +15,15 @@ const options = [
 class FinalForm extends Component {
   state = {}
 
-  handleChange = (e, { value }) => this.setState({ value })
+  handleChange = (name, e) => {
+    const change = {}
+    let inputValue = e.target.value
+    if (name === 'voornaam' || name === 'naam') inputValue = inputValue.charAt(0).toUpperCase() + inputValue.slice(1)
+    change[name] = inputValue
+    this.setState({ ...change })
+  }
+
+
 
   render() {
     const { value } = this.state
@@ -24,25 +33,27 @@ class FinalForm extends Component {
           <Logo/>
           We're sorry to see you go! We will miss you!
         </Header>
+        <UiDivider/>
         <Form>
           <Form.Group widths='equal'>
-            <Form.Input fluid label='Naam' placeholder='Naam' />
-            <Form.Input fluid label='Voornaam' placeholder='Voornaam'/>
+            <Form.Input fluid label='Naam' placeholder='Naam' onChange={e => this.handleChange('naam', e)}/>
+            <Form.Input fluid label='Voornaam' placeholder='Voornaam' onChange={e => this.handleChange('voornaam', e)}/>
           </Form.Group>
           <Form.Group widths='equal'>
-            <Form.Input fluid label='E-mail' placeholder='E-mail' />
-            <Form.Input fluid label='Telefoonnummer' placeholder='Telefoonnummer' />
+            <Form.Input fluid label='E-mail' placeholder='E-mail' onChange={e => this.handleChange('email', e)} />
+            <Form.Input fluid label='Telefoonnummer' placeholder='Telefoonnummer' onChange={e => this.handleChange('telefoonummer', e)} />
           </Form.Group>
           <Form.Group widths='equal'>
-            <Form.Input fluid label='Adres' placeholder='Adres' />
+            <Form.Input fluid label='Adres' placeholder='Adres' onChange={e => this.handleChange('adres', e)} />
           </Form.Group>
           <Form.Select
             fluid
             label='Reason to leave'
             options={options}
             placeholder='Reason'
+            onChange={e => this.setState({'reason': e.target.innerText})}
           />
-          <Form.TextArea label='About' placeholder='Tell us more about what could we do differently...' />
+          <Form.TextArea label='About' placeholder='Tell us more about what could we do differently...' onChange={e => this.handleChange('description', e)}/>
           <Form.Button>Submit</Form.Button>
         </Form>
       </div>
